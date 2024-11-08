d5522db117135b7e5205746c493da9f6a465b9e0
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-11-08 17:45:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
-  proxy.web(req, res, { target: 'http://fred.hidencloud.com:25901/webhook' });
+  proxy.web(req, res, { target: 'http://fred.hidencloud.com:25901/' });
 });
 server.listen(8080, () => {
   console.log('Proxy server is running on http://localhost:8080');
